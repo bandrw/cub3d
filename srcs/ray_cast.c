ea2eb7df4e6892d2521a1e8a2c2eb3789f4e04ce
Andreff16 <andr.b.2001@gmail.com>
2021-01-02 08:00:23 +0300

    2d optimized ray_cast with rectangles

--- srcs/ray_cast.c
+++ srcs/ray_cast.c
@@ -10,7 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/cub3d.h"
+#include "cub3d.h"
+
+static void write_rectangle(int ray_x, int ray_y, t_img *tmp)
+{
+	t_rectangle rectangle;
+
+	rectangle.start.x = (float)ray_x - 2;
+	rectangle.start.y = (float)ray_y - 2;
+	rectangle.heigth = 4;
+	rectangle.width = 4;
+	if (rectangle.start.x < 496 && rectangle.start.y < 496 && rectangle.start.x > 0 && rectangle.start.y > 0)
+		put_rectangle(tmp, &rectangle, 0xFF0000);
+}
 
 static float	ray_cast_horizontal(t_mlx *mlx_info, float angle, t_img *tmp)
 {
@@ -20,21 +32,31 @@ static float	ray_cast_horizontal(t_mlx *mlx_info, float angle, t_img *tmp)
 	int		ray_x;
 	int		ray_y;
 
-	if (sinf(angle * 3.14f / 180.f) == 0)
+	if (ft_absf(sinf(angle * 3.14f / 180.f)) < 0.001f)
 		return (-1.f);
 	distance = 0.f;
-	y_delta = mlx_info->player.position.y - (int)mlx_info->player.position.y / 50 * 50;
-	x_delta = ft_absf(50 / tanf(angle * 3.14f / 180.f));
+	// смещение в пределах одной клетки
+	y_delta = (int)mlx_info->player.position.y - (int)mlx_info->player.position.y / 50 * 50;
+	if (sinf(angle * 3.14f / 180.f) < 0)
+		y_delta = 50 - y_delta;
+	x_delta = (int)ft_absf((float)y_delta / tanf(angle * 3.14f / 180.f));
 	if (cosf(angle * 3.14f / 180.f) < 0)
 		x_delta = -x_delta;
-	distance += y_delta / sinf(angle * 3.14f / 180.f);
-	ray_x = mlx_info->player.position.x + y_delta / tanf(angle * 3.14f / 180.f);
-	ray_y = mlx_info->player.position.y - y_delta;
+	distance += ft_absf((float)y_delta / sinf(angle * 3.14f / 180.f));
+	ray_x = (int)mlx_info->player.position.x + x_delta;
+	ray_y = (int)mlx_info->player.position.y - (sinf(angle * 3.14f / 180.f) > 0 ? y_delta : -y_delta);
+	write_rectangle(ray_x, ray_y, tmp);
+	// смещение пока не встретим стену
+	x_delta = (int)(50 / ft_absf(tanf(angle * 3.14f / 180.f)));
+	if (cosf(angle * 3.14f / 180.f) < 0)
+		x_delta = -x_delta;
+	y_delta = sinf(angle * 3.14f / 180.f) > 0 ? 50 : -50;
 	while (ray_y < 500 && ray_x < 500 && ray_y > 0 && ray_x > 0 &&
-		   mlx_info->map[10 * ray_y / mlx_info->height - (sinf(angle * 3.14f / 180.f) > 0)][10 * ray_x / mlx_info->width] != '1')
+		mlx_info->map[10 * ray_y / mlx_info->height - (sinf(angle * 3.14f / 180.f) > 0)][10 * ray_x / mlx_info->width] != '1')
 	{
 		ray_x += x_delta;
-		ray_y -= sinf(angle * 3.14f / 180.f) > 0 ? 50 : -50;
+		ray_y -= y_delta;
+		write_rectangle(ray_x, ray_y, tmp);
 		distance += 50.f / ft_absf(sinf(angle * 3.14f / 180.f));
 	}
 	return (distance);
@@ -48,31 +70,33 @@ static float	ray_cast_vertical(t_mlx *mlx_info, float angle, t_img *tmp)
 	int		ray_x;
 	int		ray_y;
 
+	if (ft_absf(cosf(angle * 3.14f / 180.f)) < 0.001f)
+		return (-1.f);
 	distance = 0.f;
-	x_delta = 50;
-	if (cosf(angle * 3.14f / 180.f) < 0)
-		x_delta = -x_delta;
-	y_delta = ft_absf(50.f * tanf(angle * 3.14 / 180.f));
-	ray_x = mlx_info->player.position.x;
-	ray_y = mlx_info->player.position.y;
+	// смещение в пределах одной клетки
+	x_delta = (int)mlx_info->player.position.x - (int)mlx_info->player.position.x / 50 * 50;
+	if (cosf(angle * 3.14f / 180.f) > 0)
+		x_delta = 50 - x_delta;
+	y_delta = (int)ft_absf((float)x_delta * tanf(angle * 3.14f / 180.f));
+	if (sinf(angle * 3.14f / 180.f) < 0)
+		y_delta = -y_delta;
+	distance += ft_absf((float)x_delta / cosf(angle * 3.14f / 180.f));
+	ray_x = (int)mlx_info->player.position.x + ((cosf(angle * 3.14f / 180.f) > 0) ? x_delta : -x_delta);
+	ray_y = (int)mlx_info->player.position.y - y_delta;
+	write_rectangle(ray_x, ray_y, tmp);
+	// смещение пока не встретим стену
+	y_delta = (int)(50 * ft_absf(tanf(angle * 3.14f / 180.f)));
+	if (sinf(angle * 3.14f / 180.f) < 0)
+		y_delta = -y_delta;
+	x_delta = cosf(angle * 3.14f / 180.f) > 0 ? 50 : -50;
 	while (ray_y < 500 && ray_x < 500 && ray_y > 0 && ray_x > 0 &&
-			mlx_info->map[10 * ray_y / mlx_info->height][10 * ray_x / mlx_info->width - (cosf(angle * 3.14f / 180.f) < 0)] != '1')
+		   mlx_info->map[10 * ray_y / mlx_info->height][10 * ray_x / mlx_info->width - (cosf(angle * 3.14f / 180.f) < 0)] != '1')
 	{
 		ray_x += x_delta;
-		ray_y -= sinf(angle * 3.14f / 180.f) > 0 ? y_delta : -y_delta;
-		t_rectangle rectangle;
-		rectangle.start.x = ray_x - 2;
-		rectangle.start.y = ray_y - 2;
-		rectangle.heigth = 4;
-		rectangle.width = 4;
-		if (rectangle.start.x < 498 && rectangle.start.y < 498 && rectangle.start.x > 0 && rectangle.start.y > 0)
-			put_rectangle(tmp, &rectangle, 0xFF0000);
-		if (cosf(angle * 3.14f / 180.f) == 0)
-			return (-1);
+		ray_y -= y_delta;
+		write_rectangle(ray_x, ray_y, tmp);
 		distance += 50.f / ft_absf(cosf(angle * 3.14f / 180.f));
-		printf("%d %d\n", ray_x, ray_y);
 	}
-	printf("\n");
 	return (distance);
 }
 
@@ -83,8 +107,7 @@ float	ray_cast(t_mlx *mlx_info, float angle, t_img *tmp)
 
 	a = ray_cast_horizontal(mlx_info, angle, tmp);
 	b = ray_cast_vertical(mlx_info, angle, tmp);
-	printf("%f : %f\n", a, b);
-	if (a < 0 || b < a)
+	if (a < 0 || (b < a && b >= 0))
 		return (b);
 	return (a);
 }
